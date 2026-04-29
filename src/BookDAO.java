82d8b72952c2f6f919b6efca6ad8ae2d20b15055
sitanatarajan2006 <sitanatarajan3@gmail.com>
2026-04-29 16:45:23 +0100

    added code that saves a book into the database

--- src/BookDAO.java
+++ src/BookDAO.java
@@ -3,5 +3,17 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class BookDAO {
-
+    public void addBook(Book book) {
+        String sql = "INSERT INTO books (title, author, category, availability_status) VALUES (?, ?, ?, ?)";
+        try (Connection conn = DatabaseConnection.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, book.getTitle());
+            pstmt.setString(2, book.getAuthor());
+            pstmt.setString(3, book.getCategory());
+            pstmt.setString(4, book.getAvailabilityStatus());
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println("Error adding book: " + e.getMessage());
+        }
+    }
 }
