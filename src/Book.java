94ad07533f2d4d08cd8287e873a3710cb3b62cc1
sitanatarajan2006 <sitanatarajan3@gmail.com>
2026-04-29 16:19:05 +0100

    fixed contructor added basic getters

--- src/Book.java
+++ src/Book.java
@@ -6,4 +6,18 @@ public class Book {
     private String category;
     private String availabilityStatus;
 
+    public Book(int bookId, String title, String author, String category, String availabilityStatus) {
+        this.bookId = bookId;
+        this.title = title;
+        this.author = author;
+        this.category = category;
+    this.availabilityStatus = availabilityStatus;
+    }
+    public int getBookId() {
+    return bookId;
+}
+
+public String getTitle() {
+    return title;
+}
 }
