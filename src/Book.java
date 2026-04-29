564530dce2b48f20589498ffc6fae10b178b73a1
sitanatarajan2006 <sitanatarajan3@gmail.com>
2026-04-29 16:30:40 +0100

    completed book class with getters and setters

--- src/Book.java
+++ src/Book.java
@@ -11,22 +11,42 @@ public class Book {
         this.title = title;
         this.author = author;
         this.category = category;
-    this.availabilityStatus = availabilityStatus;
+        this.availabilityStatus = availabilityStatus;
     }
-    public int getBookId()
-    return bookId;
-}
 
-public String getTitle() {
-    return title;
-}
-public String getAuthor() {
-    return author;
-}
+    public int getBookId() {
+        return bookId;
+    }
 
-public String getCategory() {
-    return category;
-}
-public String getAvailabilityStatus() {
-    return availabilityStatus;
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getAvailabilityStatus() {
+        return availabilityStatus;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setAvailabilityStatus(String availabilityStatus) {
+        this.availabilityStatus = availabilityStatus;
+    }
 }
